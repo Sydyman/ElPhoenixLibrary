f2c011f4f02b17aae9cf3eef42701aa28b9a9757
Sydyman <Homelandertexas@gmail.com>
2025-03-20 11:39:09 +0600

    maven

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     kotlin("jvm") version "1.9.0"
-    `maven-publish`
 }
 
 group = "com.github.Sydyman"
@@ -8,24 +7,4 @@ version = "1.0.0"
 
 repositories {
     mavenCentral()
-}
-
-publishing {
-    publications {
-        create<MavenPublication>("release") {
-            groupId = "com.github.Sydyman"
-            artifactId = "ElPhoenixLibrary"
-            version = "1.0.0"
-
-            afterEvaluate {
-                from(components["kotlin"])
-            }
-        }
-    }
-
-    repositories {
-        maven {
-            url = uri("https://jitpack.io")
-        }
-    }
 }
